319c9bd2e665fba48e602937a4aed37dc3568e92
Nick <spam@coreworks.be>
2025-12-09 01:44:00 +0100

    Delete app/actions2.js

--- app/actions2.js
+++
@@ -1,7 +0,0 @@
-"use server"
-
-// Default export - does this change the module structure?
-export default async function defaultAction(formData) {
-  console.log("Default action called")
-  return { success: true }
-}
